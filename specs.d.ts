2dc6fb5638bbf3f65de3834b9699a0e00b850d27
Rio on Mac <riochr17@gmail.com>
2022-05-29 12:53:20 +0700

    checkpoint

--- specs.d.ts
+++ specs.d.ts
@@ -1,49 +1,49 @@
 import { Vertex, CoordinatedVertex, Edge } from "./src/graph.interface";
 import { FloodFill, FloodFillTree } from "./src/algorithm/flood-fill";
-import { DFS } from './build/algorithm/dfs';
-import { FloydWarshall } from './build/algorithm/floyd-warshall';
-import { ForceDirectedEades } from './build/algorithm/force-directed-eades';
-import { ForceDirectedFruchtermanReingold } from './build/algorithm/force-directed-fruchterman-reingold';
-import { ForceDirectedKamadaKawai } from './build/algorithm/force-directed-kamada-kawai';
+import { DFS } from './src/algorithm/dfs';
+import { FloydWarshall } from './src/algorithm/floyd-warshall';
+import { ForceDirectedEades } from './src/algorithm/force-directed-eades';
+import { ForceDirectedFruchtermanReingold } from './src/algorithm/force-directed-fruchterman-reingold';
+import { ForceDirectedKamadaKawai } from './src/algorithm/force-directed-kamada-kawai';
 
-declare module 'dearblues-syntax' {
+declare module 'dearblues-algorithm' {
   export {
     Vertex, CoordinatedVertex, Edge,
     FloodFillTree
   }  
 }
 
-declare module 'dearblues-syntax/build/algorithm/dfs' {
+declare module 'dearblues-algorithm/build/algorithm/dfs' {
   export {
     DFS
   }
 }
 
-declare module 'dearblues-syntax/build/algorithm/flood-fill' {
+declare module 'dearblues-algorithm/build/algorithm/flood-fill' {
   export {
     FloodFill
   }
 }
 
-declare module 'dearblues-syntax/build/algorithm/floyd-warshall' {
+declare module 'dearblues-algorithm/build/algorithm/floyd-warshall' {
   export {
     FloydWarshall
   }
 }
 
-declare module 'dearblues-syntax/build/algorithm/force-directed-eades' {
+declare module 'dearblues-algorithm/build/algorithm/force-directed-eades' {
   export {
     FloydWarshall
   }
 }
 
-declare module 'dearblues-syntax/build/algorithm/force-directed-fruchterman-reingold' {
+declare module 'dearblues-algorithm/build/algorithm/force-directed-fruchterman-reingold' {
   export {
     ForceDirectedFruchtermanReingold
   }
 }
 
-declare module 'dearblues-syntax/build/algorithm/force-directed-kamada-kawai' {
+declare module 'dearblues-algorithm/build/algorithm/force-directed-kamada-kawai' {
   export {
     ForceDirectedKamadaKawai
   }
