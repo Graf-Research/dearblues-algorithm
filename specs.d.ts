6a3aedbd4843b73ae64869c6b16ce48071687b1c
Rio on Mac <riochr17@gmail.com>
2022-05-29 12:47:15 +0700

    checkpoint

--- specs.d.ts
+++ specs.d.ts
@@ -1,7 +1,29 @@
 import { Vertex, CoordinatedVertex, Edge } from "./src/graph.interface";
-import { FloodFillTree } from "./src/algorithm/flood-fill";
+import { FloodFill, FloodFillTree } from "./src/algorithm/flood-fill";
+import { DFS } from './src/algorithm/dfs';
+import { FloydWarshall } from './src/algorithm/floyd-warshall';
 
-export {
-  Vertex, CoordinatedVertex, Edge,
-  FloodFillTree
-};
+declare module 'dearblues-syntax' {
+  export {
+    Vertex, CoordinatedVertex, Edge,
+    FloodFillTree
+  }  
+}
+
+declare module 'dearblues-syntax/build/algorithm/dfs' {
+  export {
+    DFS
+  }
+}
+
+declare module 'dearblues-syntax/build/algorithm/flood-fill' {
+  export {
+    FloodFill
+  }
+}
+
+declare module 'dearblues-syntax/build/algorithm/floyd-warshall' {
+  export {
+    FloydWarshall
+  }
+}
