0fad1ce3eb0c2d5ba5904eb4e493be8a7b82fa83
Rio on Mac <riochr17@gmail.com>
2022-05-29 12:42:30 +0700

    checkpoint

---
+++ specs.d.ts
@@ -0,0 +1,7 @@
+import { Vertex, CoordinatedVertex, Edge } from "./src/graph.interface";
+import { FloodFillTree } from "./src/algorithm/flood-fill";
+
+export {
+  Vertex, CoordinatedVertex, Edge,
+  FloodFillTree
+};
