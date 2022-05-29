bb1a11e3de0195609060a846625ccd19f2fc8664
Rio on Mac <riochr17@gmail.com>
2022-05-29 12:51:55 +0700

    checkpoint

--- specs.d.ts
+++ specs.d.ts
@@ -1,10 +1,10 @@
 import { Vertex, CoordinatedVertex, Edge } from "./src/graph.interface";
 import { FloodFill, FloodFillTree } from "./src/algorithm/flood-fill";
-import { DFS } from './src/algorithm/dfs';
-import { FloydWarshall } from './src/algorithm/floyd-warshall';
-import { ForceDirectedEades } from './src/algorithm/force-directed-eades';
-import { ForceDirectedFruchtermanReingold } from './src/algorithm/force-directed-fruchterman-reingold';
-import { ForceDirectedKamadaKawai } from './src/algorithm/force-directed-kamada-kawai';
+import { DFS } from './build/algorithm/dfs';
+import { FloydWarshall } from './build/algorithm/floyd-warshall';
+import { ForceDirectedEades } from './build/algorithm/force-directed-eades';
+import { ForceDirectedFruchtermanReingold } from './build/algorithm/force-directed-fruchterman-reingold';
+import { ForceDirectedKamadaKawai } from './build/algorithm/force-directed-kamada-kawai';
 
 declare module 'dearblues-syntax' {
   export {
